1c63be60b110a6938c8f8f97fc77b71a7ac2d591
David Malkowski <davem528@gmail.com>
2015-06-27 18:35:13 -0400

    no message

--- Creature.swift
+++ Creature.swift
@@ -9,5 +9,16 @@
 import UIKit
 
 class Creature: CCSprite {
-   
+    
+    var isAlive = false {
+        didSet {
+            self.visible = isAlive
+        }
+    }
+    var livingNeighborsCount = 0
+    
+    convenience override init() {
+        self.init(imageNamed: "GameOfLifeAssets/Assets/bubble.png")
+        anchorPoint = CGPoint(x: 0, y: 0)
+    }
 }
--- Grid.swift
+++ Grid.swift
@@ -9,5 +9,53 @@
 import UIKit
 
 class Grid: CCSprite {
+    
+    let GridRows = 8
+    let GridColumns = 10
+    
+    class Grid: CCSprite {
+        var totalAlive = 0
+        var generation = 0
+        
+        var cellWidth: CGFloat = 0
+        var cellHeight: CGFloat = 0
+        var gridArray: [[Creature]]!
+        
+        // Add Grid methods below here
+        
+        override func onEnter() {
+            super.onEnter()
+            
+            setupGrid()
+            
+            userInteractionEnabled = true
+        }
+        
+        func setupGrid() {
+            
+            cellWidth = contentSize.width / CGFloat(GridColumns)
+            cellHeight = contentSize.height / CGFloat(GridRows)
+            
+            gridArray = []
+            
+            for row in 0..<GridRows {
+                gridArray.append([])
+                
+                for column in 0..<GridColumns {
+                    var creature = Creature()
+                    creature.position = CGPoint(x: cellWidth * CGFloat(column),
+                        y: cellHeight * CGFloat(row))
+                    addChild(creature)
+                    
+                    gridArray[row].append(creature)
+                    
+                    creature.isAlive = true
+                }
+            }
+        }
+        
+    }
    
 }
+
+
