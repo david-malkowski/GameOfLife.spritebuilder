f45094f1022205401beef0a9cdea50d32276488c
David Malkowski <davem528@gmail.com>
2015-06-27 18:34:44 -0400

    Programming for Xcode has begun, problems with main.cc

---
+++ Creature.swift
@@ -0,0 +1,13 @@
+//
+//  Creature.swift
+//  
+//
+//  Created by David Emannuel Malkowski on 6/27/15.
+//
+//
+
+import UIKit
+
+class Creature: CCSprite {
+   
+}
---
+++ Grid.swift
@@ -0,0 +1,13 @@
+//
+//  Grid.swift
+//  
+//
+//  Created by David Emannuel Malkowski on 6/27/15.
+//
+//
+
+import UIKit
+
+class Grid: CCSprite {
+   
+}
